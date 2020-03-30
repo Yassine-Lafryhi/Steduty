d5fb945397f263c0cc033cc1770d02dbf937a25d
Yassine Lafryhi <y.lafryhi@gmail.com>
2020-03-30 01:50:40 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,10 +10,10 @@ $estsb_logo = get_template_directory_uri() . '/assets/images/logo_estsb.jpg';
 
         <div class="row">
             <div class="col-sm-3"  id="text_footer">
-                <h2>EST SIDI BENNOUR</h2>
-                <p>Adresse : Avenue des Facultés, El Jadida 24000.<br>
-                    Téléphone : 0600000000<br>
-                    Fax : 0500000000</p>
+                <h2><?php bloginfo( 'name' ); ?></h2>
+                <p>Adresse : <?php echo steduty_get_option( 'address_text' );?><br>
+                    Téléphone : <?php echo steduty_get_option( 'phone_text' );?><br>
+                    Fax : <?php echo steduty_get_option( 'fax_text' );?></p>
 
             </div>
             <div class="col-sm-3"  id="text_footer">
@@ -38,13 +38,14 @@ $estsb_logo = get_template_directory_uri() . '/assets/images/logo_estsb.jpg';
             <div class="col-sm-3" id="text_footer">
                 <h2>MAP</h2>
                 <p>
-<iframe src="<?php bloginfo('template_url'); ?>/assets/images/adress_est.png" width="100%" frameborder="0" style="border:0" allowfullscreen=""></iframe>                </p>
+					<a href="https://goo.gl/maps/9Fo9mvp2Yu8Cobrj8">
+					<img src="<?php bloginfo('template_url'); ?>/assets/images/adress_est.png" alt="map"/>               </a> </p>
             </div>
         </div>
 
     </div>
     <div class="wrapper wrapper-copy">
-        <p class="copy">Copyright © <?php echo date("Y"); ?> EST Sidi Bennour. All Rights Reserved. </p>
+        <p class="copy">Copyright © <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All Rights Reserved. </p>
         <p class="copy-ilovewp"><span class="theme-credit">Powered by <a href="https://github.com/Yassine-Lafryhi/Steduty"  class="footer-logo-ilovewp"  style="text-decoration:  none; font-size: 16px;">Steduty</a></span></p>
     </div>
 </footer><!-- .site-footer -->
